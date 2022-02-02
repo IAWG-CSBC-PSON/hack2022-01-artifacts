d93db859a836f143f84422ce6350ffd16f82ed9c
gjbaker <gregory_baker2@hms.harvard.edu>
2022-02-01 22:51:54 -0500

    Add pr.py

--- score/pr.py
+++ score/pr.py
@@ -5,22 +5,25 @@ from os.path import abspath
 
 def binary_pr(pred, truth):
 
-    # check that CellIDs between predictions and truth table match
+    # check that CellIDs between predictions and truth tables match
     assert set(pred['CellID']) == set(truth['CellID']), \
-        'Cell ID mismatch between predictions table and truth table.'
+        'Cell IDs mismatch between predictions table and truth table.'
 
-    # check that predictions table only contains valid class labels
+    # check that PREDICTIONS table only contains valid class labels
     assert set(pred['class_label'].unique()).issubset([0, 1]), \
-        'Predictions table contains class labels other than 0 and 1.'
+        'Predictions table contains class labels outside of 0 and 1.'
 
-    # check that truth table only contains valid class labels
+    # check that TRUTH table only contains valid class labels
     assert set(truth['class_label'].unique()).issubset([0, 1]), \
         'Truth table contains class labels other than 0 and 1.'
 
+    # merge prediction and truth tables
     merge = truth.merge(
         pred, how='inner', on='CellID', suffixes=('_truth', '_pred')
         )
 
+    # Identify true positives, true negatives,
+    # false positives, and false negatives
     true_pos = merge[
         (merge['class_label_truth'] == 1) & (merge['class_label_pred'] == 1)]
     true_neg = merge[
@@ -30,6 +33,7 @@ def binary_pr(pred, truth):
     false_neg = merge[
         (merge['class_label_truth'] == 1) & (merge['class_label_pred'] == 0)]
 
+    # compute precision and recall
     precision = round(len(true_pos)/(len(true_pos) + len(false_pos)), 2)
     recall = round(len(true_pos)/(len(true_pos) + len(false_neg)), 2)
 
@@ -44,8 +48,9 @@ pred = pd.read_csv(pred)
 truth = abspath(sys.argv[2])
 truth = pd.read_csv(truth)
 
-# compute and print accuracy, precision, and recall
+# compute precision and recall on input
 precision, recall = binary_pr(pred, truth)
 
+# print precision and recall values
 print()
 print(f'precision={precision}, recall={recall}')
